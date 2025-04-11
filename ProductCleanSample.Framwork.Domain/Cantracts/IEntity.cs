d98d48863744c3e72e1ad93a50b68d3bba77c7b0
jahangard58 <jahangard58@yahoo.com>
2025-04-11 20:20:04 +0330

    Add project files.

---
+++ ProductCleanSample.Framwork.Domain/Cantracts/IEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCleanSample.Framwork.Domain.Cantracts
+{
+    //1
+    public interface IEntity<out TId>
+    {
+        TId Id { get; }
+    }
+}
---
+++ ProductCleanSample.Framwork.Domain/Cantracts/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCleanSample.Framwork.Domain.Cantracts
+{
+    //2
+    public interface IRepository<TEntity, TId> 
+           where TEntity : IEntity<TId>
+           where TId : notnull
+    {
+        Task InsertAsync(TEntity entity);
+        Task UpdateAsync(TEntity entity);
+        Task DeleteAsync(TEntity entity);
+        Task<TEntity?> GetByIdAsync(TId id);
+    }
+}
