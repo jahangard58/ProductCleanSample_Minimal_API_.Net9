dc7ec59d6104e1d70a522dfca83e2ef39254036d
jahangard58 <jahangard58@yahoo.com>
2025-04-11 20:46:36 +0330

    Entity_Created

---
+++ ProductCleanSample.Framwork.Domain/Entity.cs
@@ -0,0 +1,16 @@
+﻿using ProductCleanSample.Framwork.Domain.Cantracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCleanSample.Framwork.Domain
+{
+    public abstract class Entity<TId>: IEntity<TId>
+        where TId : notnull
+    {
+        public TId Id { get;}
+        protected Entity(TId id) => Id = id;
+    }
+}
