b02eef082938746e9e495503f3d292693fc05ff1
jahangard58 <jahangard58@yahoo.com>
2025-04-11 20:39:16 +0330

    SearchData_Added

---
+++ ProductCleanSample.Framwork.Domain/Cantracts/Data/SearchData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCleanSample.Framwork.Domain.Cantracts.Data
+{
+    public record SearchData(string? SearchText,string? Sort,int? PageSize,int? PageIndex);
+    
+}
