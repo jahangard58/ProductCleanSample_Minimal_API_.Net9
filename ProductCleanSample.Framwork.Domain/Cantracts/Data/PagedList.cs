e3d8d8ac6d9f040aaad5d4c902ac5b24418297fb
jahangard58 <jahangard58@yahoo.com>
2025-04-11 20:35:34 +0330

    PagedList_Added

---
+++ ProductCleanSample.Framwork.Domain/Cantracts/Data/PagedList.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCleanSample.Framwork.Domain.Cantracts.Data
+{
+    public class PagedList<TEntity>(IEnumerable<TEntity> entities,int totalRecordCount)
+    {
+        public IReadOnlyList<TEntity> Entities => entities.ToList().AsReadOnly();
+
+        public int TotalRecordCount => totalRecordCount;
+    }
+}
