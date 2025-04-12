252d44e5e9bbdbb5f3cb6e4ede41f520354bc647
jahangard58 <jahangard58@yahoo.com>
2025-04-12 11:45:50 +0330

    SqlRepository_Class_Added

---
+++ ProductCleanSample.Framework.Infrastructure/Data/SqlRepository.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductCleanSample.Framwork.Domain;
+using ProductCleanSample.Framwork.Domain.Cantracts;
+using ProductCleanSample.Framwork.Domain.Cantracts.Data;
+using System.Linq.Expressions;
+
+namespace ProductCleanSample.Framework.Infrastructure.Data
+{
+    public abstract class SqlRepository<TEntity,TId> : IRepository<TEntity, TId>
+        where TEntity : Entity<TId>
+        where TId : notnull
+    {
+        private readonly DbContext context;
+        protected readonly DbSet<TEntity> entities;
+
+        
+        protected SqlRepository(DbContext dbContext)
+        {
+            this.context = dbContext;
+            this.entities = dbContext.Set<TEntity>();   
+        }
+
+        public async Task InsertAsync(TEntity entity)
+        {
+            await entities.AddAsync(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            entities.Update(entity);
+            await context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(TEntity entity)
+        {
+            entities.Remove(entity);
+            await context.SaveChangesAsync();
+        }
+
+
+        //? Public Search (Reflection)
+        public Task<PagedList<TEntity>> SearchAsync(SearchData data)
+        {
+            //? DRY  تکرار نکن
+            return Search(entities.AsNoTracking(), data.SearchText)
+                .Sort(data.Sort)
+                .PaginateAsync(data.PageSize, data.PageIndex);
+        }
+        protected abstract IQueryable<TEntity> Search(IQueryable<TEntity> query, string? searchText);
+
+
+        public async Task<IReadOnlyList<TEntity>> FindAsync(Expression<Func<TEntity, bool>> predicate)
+        {
+            return await entities.AsNoTracking().Where(predicate).ToListAsync();
+        }
+
+        public async Task<TEntity?> GetByIdAsync(TId id)
+        {
+            var entity = await entities.FindAsync(id);
+
+            return entity;
+        }
+
+        
+    }
+
+}
--- ProductCleanSample.Framwork.Domain/Cantracts/Data/SearchData.cs
+++ ProductCleanSample.Framwork.Domain/Cantracts/Data/SearchData.cs
@@ -6,6 +6,6 @@ using System.Threading.Tasks;
 
 namespace ProductCleanSample.Framwork.Domain.Cantracts.Data
 {
-    public record SearchData(string? SearchText,string? Sort,int? PageSize,int? PageIndex);
+    public record SearchData(string? SearchText,string? Sort,int PageSize,int PageIndex);
     
 }
