2d44697bc85b41741129d423fee1deb8a39d9d55
jahangard58 <jahangard58@yahoo.com>
2025-04-12 10:48:40 +0330

    FramworkApplication_Created

---
+++ ProductCleanSample.Framwork.Application/Contracts/IIdGenerator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCleanSample.Framwork.Application.Contracts
+{
+    public interface IIdGenerator<TId>
+    {
+        TId Next();
+    }
+}
---
+++ ProductCleanSample.Framwork.Application/Contracts/INotification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductCleanSample.Framwork.Application.Contracts
+{
+    public interface INotification
+    {
+        Task SendAsync(string message,string destination);
+    }
+}
